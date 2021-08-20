556f96354d870ba02de4a264c25bea8d2f927874
maheeashah <maheeashah@hotmail.com>
2021-08-20 08:18:18 -0500

    display welcome activity and hard coding methods

---
+++ app/src/main/java/com/example/userinput/DispayWelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.userinput;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DispayWelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispay_welcome);
+    }
+}
--- app/src/main/java/com/example/userinput/MainActivity.java
+++ app/src/main/java/com/example/userinput/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.userinput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+/* Anytime a method is to be called from a button click. it MUST HAVE a specific type
+ of method signature. If you don't use the right signature, your button will NOT be
+  allowed to call this method.
+
+
+  public void methodName(View v) {
+  }
+  View is a class that you need to import
+
+ */
+
+    public void makeName(View v) {
+        EditText firstNameET = findViewById(R.id.firstNameEditText);
+        EditText lastNameET = findViewById(R.id.lastNameEditText);
+
+        String firstName = firstNameET.getText().toString();
+        String lastName = lastNameET.getText().toString();
+
+        String fullName = firstName + " " + lastName;
+        TextView fullNameTextView = findViewById(R.id.completeNameTextView);
+        fullNameTextView.setText(fullName);
     }
 }
